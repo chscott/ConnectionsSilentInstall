3e95b7fc51b6ab6df22d593805fed0ea4a38d780
Chad Scott <chads@us.ibm.com>
2017-06-13 10:23:25 -0500

    Changed some logging

--- src/ic/post_install.sh
+++ src/ic/post_install.sh
@@ -9,48 +9,48 @@
 propagatePluginKeystore="${stagingDir}/src/web/propagate_keys.sh"
 syncNode="${ic1ProfileDir}/bin/syncNode.sh ${fqdn} -username ${dmgrAdminUser} -password ${defaultPwd}"
 
-log "INSTALL: Beginning Connections post-install tasks..."
+log "I Beginning Connections post-install tasks..."
 
 # Do initialization stuff
 init ${icStagingDir} configure 
 
 # Propagate the plug-in keystore 
 ${propagatePluginKeystore}
-checkStatus ${?} "ERROR: Unable to propagate plug-in keystore. Exiting."
+checkStatus ${?} "E Unable to propagate plug-in keystore. Exiting."
 
 # Restart the web server to load the updated plug-in
 result=$(restartIHSServer)
 if [ ${result} -ne 0 ]; then
-    log "WARNING: Manual IHS restart required. Continuing..."
+    log "W Manual IHS restart required. Continuing..."
 fi
 
 # Make sure the Connections application server is stopped
 result=$(stopWASServer ${ic1ServerName} ${ic1ProfileDir})
-checkStatus ${result} "ERROR: Unable to stop the Connections application server. Exiting."
+checkStatus ${result} "E Unable to stop the Connections application server. Exiting."
 
 # Make sure the node agent is stopped 
 result=$(stopWASServer nodeagent ${ic1ProfileDir})
-checkStatus ${result} "ERROR: Unable to stop node agent. Exiting."
+checkStatus ${result} "E Unable to stop node agent. Exiting."
 
 # Make sure the deployment manager is running
 result=$(startWASServer ${dmgrServerName} ${dmgrProfileDir})
-checkStatus ${result} "ERROR: Unable to start the deployment manager. Exiting"
+checkStatus ${result} "E Unable to start the deployment manager. Exiting"
 
 # Run a node agent sync
-log "Synchronizing node..."
+log "I Synchronizing node..."
 ${syncNode} >>${scriptLog} 2>&1
-checkStatus ${?} "ERROR: Unable to synchronize the node. Exiting."
+checkStatus ${?} "E Unable to synchronize the node. Exiting."
 
 # Restart the deployment manager
 result=$(restartWASServer ${dmgrServerName} ${dmgrProfileDir})
-checkStatus ${result} "ERROR: Unable to restart the deployment manager. Exiting."
+checkStatus ${result} "E Unable to restart the deployment manager. Exiting."
 
 # Start the node agent
 result=$(startWASServer nodeagent ${ic1ProfileDir})
-checkStatus ${result} "ERROR: Unable to start node agent. Exiting."
+checkStatus ${result} "E Unable to start node agent. Exiting."
 
 # Restart the Connections application server
 result=$(restartWASServer ${ic1ServerName} ${ic1ProfileDir})
-checkStatus ${result} "ERROR: Unable to restart the Connections application server. Exiting."
+checkStatus ${result} "E Unable to restart the Connections application server. Exiting."
 
-log "INSTALL: Success! Connections post-install tasks completed."
+log "I Success! Connections post-install tasks completed."
